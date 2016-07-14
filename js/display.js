60f75355ac934e646f188ca08cec37c5f4e30a3d
meg768 <magnus@egelberg.se>
2016-07-14 21:14:54 +0200

    -

--- js/display.js
+++ js/display.js
@@ -4,7 +4,9 @@ var matrix = new Matrix({width:32, height:32});
 var Display = function() {
 
 	this.text = function(text) {
-		matrix.runText(text, {fontName: 'fonts/Arial-Black.ttf'});
+		var options = {};
+		options.fontName = 'fonts/Arial-Bold.ttf';
+		matrix.runText(text, options);
 	}
 
 };
