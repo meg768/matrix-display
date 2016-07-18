637fc16016efc006017da0f4e989db2966e6d418
meg768 <magnus@egelberg.se>
2016-07-18 11:46:40 +0200

    -

--- commands/test2.js
+++ commands/test2.js
@@ -15,7 +15,7 @@ function callback2() {
 //setTimeout(callback2, 6000);
 
 var rule    = new schedule.RecurrenceRule();
-rule.second = 10;
+rule.second = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
 
 schedule.scheduleJob(rule, function() {
 	console.log('...');
