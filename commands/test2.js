513e641117a66a9f391a37beab87d4d8f7dcc4c2
meg768 <magnus@egelberg.se>
2016-07-18 11:17:20 +0200

    -

--- commands/test2.js
+++ commands/test2.js
@@ -1,28 +1,18 @@
 var Matrix = require('hzeller-matrix');
 var matrix = new Matrix({width:32, height:32});
 
-function callback1(){
-
-	console.log('Starting callback1...');
-
-	//setTimeout(callback2, 6000);
-	matrix.runText('callback1',{textColor: 'blue', delay:10}, function() {
-		console.log('Callback1 completed.'); // This works fine too, image does scroll
+function callback1() {
+	// setTimeout(callback2, 6000);
+	matrix.runText('callback1',{textColor: 'blue', delay:10}, function(){
+		console.log('Scroll Text Complete');
 		callback2();
 	});
 }
-function callback2() {
-	console.log('Starting callback2.');
 
-	matrix.runText('callback2', {}, function() { //Does not fire/display anything
-		console.log('Callback2 completed'); //does fire
+function callback2(){
+	console.log('pixel update Complete');
+	matrix.runText('callback2',{textColor: 'green', delay:10}, function(){
+		console.log('Scroll Text Complete');
 	});
 }
-
-matrix.runText('main',{},callback1); //This works fine
-
-/*
-matrix.runText('HELLO!', {}, function(){
-	console.log('doneä');
-});
-*/
+matrix.runText('main',{},callback1);
