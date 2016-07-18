86aa6195805e11b8327277d0964977cd2c7cd838
meg768 <magnus@egelberg.se>
2016-07-18 11:50:27 +0200

    -

--- commands/test2.js
+++ commands/test2.js
@@ -4,15 +4,15 @@ var matrix = new Matrix({width:32, height:32});
 var schedule = require('node-schedule');
 
 function callback1() {
-	matrix.runText('callback1');
+	matrix.runImage('../images/emoji.png');
 }
 
 function callback2() {
-	matrix.runText('callback2');
+	matrix.runImage('../images/emoji.png');
 }
 
-setTimeout(callback1, 1000);
-setTimeout(callback2, 6000);
+setTimeout(callback1, 5000);
+setTimeout(callback2, 15000);
 
 var rule    = new schedule.RecurrenceRule();
 rule.second = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
