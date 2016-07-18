02b279f4c37db340acb7ea85deeeff9380abf23a
meg768 <magnus@egelberg.se>
2016-07-18 11:54:52 +0200

    -

--- commands/test2.js
+++ commands/test2.js
@@ -5,10 +5,12 @@ var schedule = require('node-schedule');
 
 function callback1() {
 	matrix.display.drawPixel(0, 0, 255, 0, 0);
+	matrix.display.update();
 }
 
 function callback2() {
 	matrix.display.drawPixel(1, 1, 0, 0, 255);
+	matrix.display.update();
 }
 
 setTimeout(callback1, 5000);
