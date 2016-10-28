0a4e21e00145bc97ce776b300cf51d7a7a722128
meg768 <magnus@egelberg.se>
2016-10-28 22:25:11 +0200

    -

--- test.js
+++ test.js
@@ -14,7 +14,7 @@ var App = function() {
 	prefixLogs();
 
 
-	var socket = require('socket.io-client')('http://app-o.se:3000/matrix-display');
+	var socket = require('socket.io-client')('http://app-o.se:3000/matrix-displayX');
 
 
 	socket.emit('text', {text:'Magnus', fontSize:30, textColor:'blue'});
