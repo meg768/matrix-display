7411fa8f71f3abb43c8b4b0676f7e7cab8dd0007
meg768 <magnus@egelberg.se>
2016-10-28 16:03:01 +0200

    -

--- app.js
+++ app.js
@@ -1,65 +1,69 @@
+#!/usr/bin/env node
 
-var args         = require('minimist')(process.argv.slice(2));
-var fs           = require('fs');
-var sprintf      = require('yow').sprintf;
-var isString     = require('yow').isString;
-var mkdir        = require('yow').mkdir;
-var fileExists   = require('yow').fileExists;
-var prefixLogs   = require('yow').prefixLogs;
+var fs = require('fs');
+var Path = require('path');
+var mkpath = require('yow').mkpath;
+var sprintf = require('yow').sprintf;
+var isObject = require('yow').isObject;
 var redirectLogs = require('yow').redirectLogs;
+var prefixLogs = require('yow').prefixLogs;
+var cmd = require('commander');
+var Matrix = require('hzeller-matrix');
+var matrix = new Matrix({width:32, height:32});
+
+var Display = function() {
+
+	this.text = function(text) {
+		var options = {};
+		options.fontName = 'fonts/Arial-Bold.ttf';
+		options.delay    = 20;
+		matrix.runText(text, options);
+	}
+
+};
 
 var App = function() {
 
-	this.run = function() {
+	cmd.version('1.0.0');
+	cmd.option('-l --log', 'redirect logs to file');
+	cmd.option('-h --host <host>', 'connect to specified server', 'localhost');
+	cmd.option('-p --port <port>', 'connect to specified port', 3001);
+	cmd.parse(process.argv);
 
-		prefixLogs();
+	prefixLogs();
 
-		try {
+	if (cmd.log) {
+		var date = new Date();
+		var path = sprintf('%s/logs', __dirname);
+		var name = sprintf('%04d-%02d-%02d-%02d-%02d-%02d.log', date.getFullYear(), date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds());
 
-			if (process.argv.length > 2) {
-				var match = process.argv[2].match('--(.+)');
-				var args = require('minimist')(process.argv.slice(2));
+		mkpath(path);
+		redirectLogs(Path.join(path, name));
+	}
+
+	var url = sprintf('http://%s:%d', cmd.host, cmd.port);
+	var socket = require('socket.io-client')(url);
+
+	console.log('Connecting to %s...', url);
+
+	socket.on('text', function(args) {
+		matrix.runText(args.text, args);
+	});
 
 
-				if (args.log) {
-					delete args.log;
-					redirectLogs();
-				}
+	socket.on('hello', function(data) {
+		var options = {};
+		options.service   = 'matrix-display';
+		options.messages  = ['text'];
+		options.events    = [];
 
-				if (match) {
-					var cmd = match[1];
-					var fileName = sprintf('./commands/%s.js', cmd);
+		console.log('Registering matrix-display...');
+		socket.emit('register', options);
+	})
+	
+	matrix.runText('Ready!');
 
-					if (fileExists(fileName)) {
-						var Module = require(fileName);
-						var module = new Module(args);
 
-						// Execute 'run' method if it has one
-						if (typeof module.run == 'function')
-							module.run();
-
-					}
-					else {
-						console.error('Command %s not found.', cmd);
-					}
-
-				}
-
-			}
-		}
-		catch (error) {
-			if (false && error instanceof Error) {
-				console.error(error.message);
-			}
-			else {
-				console.error((error && error.stack) ? error.stack : error);
-
-			}
-			process.exit(-1);
-		};
-	};
 };
 
-
-var app = new App();
-app.run();
+new App();
